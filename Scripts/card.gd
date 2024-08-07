3accba900a4b57e53bbd4af71eb4c312262d9db0
Koby Woytowicz <koby.woytowicz@gmail.com>
2024-08-07 19:27:47 -0400

    Hand Features
    added hand features:
    adding/removing cards
    fanning out cards
    highlighting selected cards
    
    also fixed memory leak

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -1,5 +1,8 @@
+@tool
 class_name Card extends Node2D
 
+signal mouse_entered(card: Card)
+signal mouse_exited(card: Card)
 @export var card_name: String = "Card Name"
 @export var card_description: String = "Card Description"
 @export var card_cost: int = 1
@@ -18,6 +21,20 @@ func _ready():
 	set_card_values(card_cost, card_name, card_description, card_cost_resource, card_image, card_background)
 	
 
+func _update_graphics():
+	if name_label.get_text() != card_name:
+		name_label.set_text(card_name)
+	if description_label.get_text() != card_description:
+		description_label.set_text(card_description)
+	if cost_label.get_text() != str(card_cost):
+		cost_label.set_text(str(card_cost))
+	if cost_sprite.texture != card_cost_resource:
+		cost_sprite.texture = card_cost_resource
+	if image_sprite.texture != card_image:
+		image_sprite.texture = card_image
+	if base_sprite.texture != card_background:
+		base_sprite.texture = card_background
+
 func set_card_values(_cost: int, _name: String, _description: String, _resource: Texture2D, _image: Texture2D, _background: Texture2D):
 	
 	card_name = _name
@@ -27,13 +44,30 @@ func set_card_values(_cost: int, _name: String, _description: String, _resource:
 	card_image = _image
 	card_background = _background
 	
-	name_label.set_text(_name)
-	description_label.set_text(_description)
-	cost_label.set_text(str(_cost))
-	cost_sprite.texture = _resource
-	image_sprite.texture = _image
-	base_sprite.texture = _background
+	_update_graphics()
 	
+	
+	
+func highlight():
+	base_sprite.set_modulate(Color(1,0,0,1))
+	
+func unhighlight():
+	base_sprite.set_modulate(Color(1,1,1,1))
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	_update_graphics()
+	
+
+
+func _on_area_2d_mouse_entered():
+	mouse_entered.emit(self)
+
+
+func _on_area_2d_mouse_exited():
+	mouse_exited.emit(self)
+
+
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	pass # Replace with function body.
--- Scripts/deck_n_hand.gd
+++ Scripts/deck_n_hand.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var strike_card: PackedScene = preload("res://Card Scenes/strike.tscn")
 @onready var dodge_card: PackedScene = preload("res://Card Scenes/dodge.tscn")
-@onready var spawn_point = $"CanvasLayer/Spawn Point"
+@onready var hand: Hand = $CanvasLayer/Hand
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,13 +16,13 @@ func _process(delta):
 
 func _on_card_spawn_tester_0_pressed():
 	var card = strike_card.instantiate()
-	spawn_point.add_child(card)
+	hand.add_card(card)
 	
 	
 
 
 func _on_card_spawn_tester_1_pressed():
 	var card = dodge_card.instantiate()
-	spawn_point.add_child(card)
+	hand.add_card(card)
 	
 
